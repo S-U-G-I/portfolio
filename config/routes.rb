b95f825037b8afce03acf1f42f765851c86c5729
Mac-User <mac-user@makku-yuzanoMacBook-puro.local>
2020-04-08 15:51:51 +0900

    フォロー機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :tweets do
     resources :comments, only: [:create, :destroy]
   end
