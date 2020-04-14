aa5196dd4d4b44b6ce2cc05bb552993d93478f1d
Mac-User <mac-user@makku-yuzanoMacBook-puro.local>
2020-04-14 17:09:08 +0900

    CSS微調整、ページネーション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all.order(id: :desc)
+    @users = User.all.order(id: :desc).page(params[:page]).per(12)
   end
 
   def show
     @user = User.find(params[:id])
+    @tweet = @user.tweets.order(id: :desc).page(params[:page]).per(9)
   end
 
   def following
     #@userがフォローしているユーザー
     @user  = User.find(params[:id])
-    @users = @user.following
+    @users = @user.following.order(id: :desc).page(params[:page]).per(12)
   end
 
   def followers
     #@userをフォローしているユーザー
     @user  = User.find(params[:id])
-    @users = @user.followers
+    @users = @user.followers.order(id: :desc).page(params[:page]).per(12)
   end
   
 end
